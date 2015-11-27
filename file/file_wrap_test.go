15758812d010cf684c1a84d0e5280a961fc3264d
Jonas Weber <mail@jonasw.de>
2015-11-27 14:44:30 +0100

    More tests

---
+++ file/file_wrap_test.go
@@ -0,0 +1,29 @@
+package file
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	wrap "github.com/thriqon/involucro/steps/wrap"
+	"testing"
+)
+
+func TestWrapTaskDefinition(t *testing.T) {
+	Convey("Given an empty runtime environment", t, func() {
+		inv := InstantiateRuntimeEnv(".")
+		env := inv.duk
+
+		runCode := func(s string) func() {
+			return func() {
+				env.EvalString(s)
+			}
+		}
+		Convey("When I define a wrap task", func() {
+			runCode(`inv.task('w').wrap("dist").inImage("p").at("/data").as("test/one")`)()
+			Convey("Then it has a corresponding entry in the task map", func() {
+				So(inv.Tasks["w"], ShouldNotBeEmpty)
+				So(inv.Tasks["w"][0], ShouldHaveSameTypeAs, wrap.WrapAsImage{})
+				wi := inv.Tasks["w"][0].(wrap.WrapAsImage)
+				So(wi.ParentImage, ShouldResemble, "p")
+			})
+		})
+	})
+}
