093e3a5650fe457210490c7771a5de449598cc93
Jonas Weber <mail@jonasw.de>
2015-12-04 15:38:14 +0100

    Expand demo file

--- examples/compile_and_run/invfile.lua
+++ examples/compile_and_run/invfile.lua
@@ -3,9 +3,11 @@ inv.task('compile')
 	.using('sojournlabs/gcc').run('gcc', '-o', 'dist/add', 'add.c', '-static')
 
 inv.task('package')
-	.wrap('dist').inImage('busybox').at('/usr/local/bin').as('test/showaddition:v1')
+	.wrap('dist').inImage('busybox').at('/usr/local/bin')
+  .withConfig({entrypoint = {"/usr/local/bin/add"}})
+  .as('test/showaddition:v1')
 
 inv.task('run')
   .using('test/showaddition:v1')
   .withExpectation({code = 0, stdout = "5 \\+ 10 = 15\n"})
-	.run('/usr/local/bin/add')
+	.run()
