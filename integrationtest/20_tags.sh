9eef245021a42d5aaedcb8c725a7c67fe9e7c413
Jonas Weber <mail@jonasw.de>
2015-12-04 13:07:45 +0100

    Modify existing integration tests

--- integrationtest/20_tags.sh
+++ integrationtest/20_tags.sh
@@ -6,9 +6,10 @@ docker rmi inttest/20
 docker rmi inttest/20:v1
 
 set -e
-$INV -e "inv.task('package').wrap('.').at('/data').as('inttest/20:v1')" package
-
+$INV -e "inv.task('package').wrap('.').inImage('busybox').at('/data').as('inttest/20:v1')" package
 docker inspect -f "{{.ID}}" inttest/20:v1
+
+$INV -e "inv.task('run').using('inttest/20:v1').run('true')" run
 
 set +e
 docker rmi inttest/20
