c4c272ade5043f262a37241063da777299ae1271
Jonas Weber <mail@jonasw.de>
2015-12-07 14:55:30 +0100

    Remove old test

--- integrationtest/04_parameters.sh
+++ integrationtest/04_parameters.sh
@@ -11,9 +11,6 @@ rm -f __asd
 ../involucro -n -e "inv.task('touch').using('busybox').run('touch', '/source/__asd')" touch
 test ! -f "__asd"
 
-../involucro -s -e "inv.task('touch').using('busybox').run('touch', '/source/__asd')" touch
-test ! -f "__asd"
-
 set +e
 ../involucro -n -e "inv.task('touch').using('busybox').run('touch', '/source/__asd')" -f invfile.js touch && { echo "Accepted -e ... -f" ; exit 1; }
 set -e
