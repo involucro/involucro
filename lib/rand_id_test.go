b38092eb8072dea71233f873e9ebdf6304dc1093
Jonas Weber <mail@jonasw.de>
2015-12-07 13:21:28 +0100

    Use examples instead of explicit tests

--- lib/rand_id_test.go
+++ lib/rand_id_test.go
@@ -1,30 +1,18 @@
 package lib
 
 import (
-	. "github.com/smartystreets/goconvey/convey"
-	"testing"
+	"fmt"
 )
 
-func TestRandomIds(t *testing.T) {
-	Convey("Given a random identifier", t, func() {
-		s := RandomIdentifier()
-		Convey("Then it is a string", func() {
-			So(s, ShouldHaveSameTypeAs, "asd")
-		})
-		Convey("When I generate another identifier", func() {
-			t := RandomIdentifier()
-			Convey("Then it is different from the original", func() {
-				So(s, ShouldNotResemble, t)
-			})
-		})
-	})
+func ExampleRandomIdentifier_AreDifferent() {
+	a := RandomIdentifier()
+	b := RandomIdentifier()
+	fmt.Println(a == b)
+	// Output: false
 }
 
-func TestRandomIdsWithLength(t *testing.T) {
-	Convey("Given a random identifier of length 64", t, func() {
-		s := RandomIdentifierOfLength(64)
-		Convey("Then it has length 64", func() {
-			So(s, ShouldHaveLength, 64)
-		})
-	})
+func ExampleRandomIdentifierOfLength() {
+	s := RandomIdentifierOfLength(64)
+	fmt.Println(len(s))
+	// Output: 64
 }
