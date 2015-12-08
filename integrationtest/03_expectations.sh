8e41e99be731f8a0bcfbb3932b9d0c17adb47c7e
Jonas Weber <mail@jonasw.de>
2015-12-08 12:09:27 +0100

    Fail when actual exit code is not expected

--- integrationtest/03_expectations.sh
+++ integrationtest/03_expectations.sh
@@ -9,6 +9,12 @@ set +e
 $INV -e "inv.task('test').using('busybox').withExpectation({stdout = 'Hello, World!\n'}).run('echo', 'Hello, Moon')" test && { echo "Accepted output..."; exit 1; }
 set -e
 
+$INV -e "inv.task('test').using('busybox').withExpectation({code = 1}).run('false')" test
+
+set +e
+$INV -e "inv.task('test').using('busybox').withExpectation({code = 1}).run('true')" test && { echo "Accepted output..."; exit 1; }
+set -e
+
 exit 0
 
 
