0040f91843e2973df38e01424cb32468a9ba3c97
Jonas Weber <mail@jonasw.de>
2016-01-14 15:59:31 +0100

    Rewrite test 27

--- integrationtest/27_values_from_cli.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-INV=$(pwd)/../involucro
-
-set -e
-$INV -vve "inv.task('test').using('busybox').run('/bin/echo', VAR['k'])" -s k=asd test 2>&1| grep "stdout: asd"
-$INV -vve "inv.task('test').using('busybox').run('/bin/echo', VAR['k'])" --set k=asd test 2>&1| grep "stdout: asd"
-$INV -vve "inv.task('test').using('busybox').run('/bin/echo', VAR['k'])" --set k=asd=6 test 2>&1| grep "stdout: asd=6"
-
