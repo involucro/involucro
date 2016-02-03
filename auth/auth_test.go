0e63f54898c789df28b358a564e65b50c024fa70
Jonas Weber <github@jonasw.de>
2016-02-03 20:16:58 +0100

    Provide means to authenticate users

---
+++ auth/auth_test.go
@@ -0,0 +1,100 @@
+package auth
+
+import (
+	"fmt"
+	"net/url"
+	"strings"
+	"testing"
+)
+
+var source = `{
+		"auths": [
+			"https://user:pw@blubb.de",
+			"https://alice:a11c3@blah.de?email=test@example.com",
+			"https://a:b@index.docker.io/v1/"
+		]
+	}`
+
+func TestParseUserInfo(t *testing.T) {
+	u, _ := url.Parse("https://test+asd:pw@quay.io")
+	if u.User.Username() != "test+asd" {
+		t.Error("user is wrong", u.User.Username())
+	}
+	if pw, set := u.User.Password(); !set || pw != "pw" {
+		t.Error("password is wrong", pw)
+	}
+}
+
+func TestGetAllFrom(t *testing.T) {
+	expected := []string{
+		"[user/pw: @ blubb.de]",
+		"[alice/a11c3:test@example.com @ blah.de]",
+		"[a/b: @ index.docker.io/v1/]",
+	}
+
+	urls, err := getAllFrom(strings.NewReader(source))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(expected) != len(urls) {
+		t.Fatal("length not equal")
+	}
+
+	for index := range expected {
+		actual := fmt.Sprintf("[%s/%s:%s @ %s]",
+			urls[index].Username, urls[index].Password, urls[index].Email, urls[index].ServerAddress)
+
+		if expected[index] != actual {
+			t.Errorf("Unexpected result for index %v: %v", index, urls[index])
+		}
+	}
+}
+
+func TestForServerInFile(t *testing.T) {
+	cases := []struct {
+		server   string
+		expected string
+		found    bool
+	}{
+		{"blah.de", "[alice/a11c3:test@example.com @ blah.de]", true},
+		{"blee.de", "[/: @ ]", false},
+		{"", "[a/b: @ https://index.docker.io/v1/]", true},
+		{"index.docker.io/v1/", "[a/b: @ https://index.docker.io/v1/]", true},
+	}
+
+	for i, el := range cases {
+		auth, foundAuthentication, err := forServerInFile(el.server, strings.NewReader(source))
+		if err != nil {
+			t.Error("in case", i, err)
+			continue
+		}
+
+		actual := fmt.Sprintf("[%s/%s:%s @ %s]",
+			auth.Username, auth.Password, auth.Email, auth.ServerAddress)
+
+		if el.expected != actual {
+			t.Errorf("unexpected result for index %v: %v", i, auth)
+		}
+
+		if foundAuthentication != el.found {
+			t.Errorf("expected found authentication to be %t, but was %t in case %v", el.found, foundAuthentication, i)
+		}
+	}
+}
+
+func TestWithFailingURLs(t *testing.T) {
+	cases := []string{
+		"\"http://withoutuser.com\"",
+		"5",
+		"\":example.com:90/\"",
+	}
+
+	for _, el := range cases {
+		file := `{"auths": [` + el + `]}`
+		_, _, err := forServerInFile("", strings.NewReader(file))
+		if err == nil {
+			t.Error("expected error in case ", el)
+		}
+	}
+}
