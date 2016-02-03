2c0dab3906b16ac4e54d385347af5fc16a2abd58
Jonas Weber <github@jonasw.de>
2016-02-03 20:59:37 +0100

    Use Push functionality from involucro in Travis Build

--- invfile.lua
+++ invfile.lua
@@ -1,10 +1,30 @@
 
+local repo = 'involucro/tool'
+
 inv.task('wrap-yourself')
 	.using('busybox:latest')
 		.run('mkdir', '-p', 'dist/tmp/')
 		.run('cp', 'involucro', 'dist/')
 	.wrap('dist').at('/')
 		.withConfig({entrypoint = {'/involucro'}})
-		.as(VAR.REPO .. ':' .. VAR.TAG)
+		.as(repo .. ':latest')
 	.using('busybox:latest')
 		.run('rm', '-rf', 'dist')
+
+
+if ENV.TRAVIS_PULL_REQUEST == "false" then
+	local tag = ENV.TRAVIS_TAG
+
+	if tag == "" then
+		tag = ENV.TRAVIS_BRANCH
+	end
+
+	if tag == "master" then
+		tag = "latest"
+	end
+
+	inv.task('upload-to-hub')
+		.tag(repo .. ':latest')
+			.as(repo .. ':' .. tag)
+		.push(repo .. ':' .. tag)
+end
